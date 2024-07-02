5981e4b925ae4e2eb57d298cf929b3afd3e273ad
nguyenphong0997 <nguyenphong@escorti.com.vn>
2024-07-02 17:00:48 +0700

    Initial Commit

---
+++ SwiftChatGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftChatGroup
+//
+//  Created by nguyenphong on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
